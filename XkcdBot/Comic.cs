a110577b84832347c3171a80abbfa9a3d615f147
dap <dap@dap.com>
2022-10-11 18:02:36 +0200

    Small quick fix for a possible bug

--- XkcdBot/Comic.cs
+++ XkcdBot/Comic.cs
@@ -9,7 +9,7 @@ public class Comic
     [JsonProperty("img")] public string? Image;
     [JsonProperty("alt")] public string? Alt;
 
-    [JsonProperty("num")] private string? _id;
+    [JsonProperty("num")] private string? _id = "1";
     public string Url => $"https://www.xkcd.com/{_id}";
     
     [JsonProperty("year")] private string? _year = "1000";
